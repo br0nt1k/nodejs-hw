3ef559cb67134360d9d106b8a9d14979f5c71952
Oleh Oprysk <opryskoleh2006@gmail.com>
2025-12-07 02:28:43 +0200

    finished hw

--- eslint.config.js
+++ eslint.config.js
@@ -3,15 +3,5 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.node },
-    rules: {
-      semi: "error",
-      "no-unused-vars": ["error", { args: "none" }],
-      "no-undef": "error",
-    },
-  },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
 ]);
--- src/server.js
+++ src/server.js
@@ -1,56 +1,26 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import pino from 'pino-http';
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import logger from './middleware/logger.js'
+import notesRouter from './routes/notesRoutes.js'
+import {errorHandler} from './middleware/errorHandler.js'
 
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+export const app = express();
+export const PORT = process.env.PORT || 3000;
 
-app.use(cors());
-app.use(express.json());
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
+app.use(logger)
+app.use(express.json())
+app.use(cors())
 
+app.use(notesRouter)
 
-app.get('/notes', (req, res) => {
-  res.status(200).json({ message: "Retrieved all notes" });
-});
+app.use(notFoundHandler);
+app.use(errorHandler)
 
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
-});
-
-app.get('/test-error', () => {
-  throw new Error('Simulated server error');
-
-});
-
-app.use((req, res) => {
-  res.status(404).json({ message: "Route not found" });
-});
-
-app.use((err, req, res, next) => {
-  console.error('Error:', err.message);
-  res.status(500).json({
-    message: 'Internal Server Error',
-    error: err.message,
-  });
-});
+await connectMongoDB()
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
